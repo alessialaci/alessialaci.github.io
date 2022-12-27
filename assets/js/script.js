7698c923a8d9f2e0c550ae7ce8c33f5b5ec85146
Alessia Lacitignola <alelaci21@gmail.com>
2022-12-27 13:05:52 +0100

    descriptions added

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,15 +22,15 @@ function printData() {
             progetti.reverse().map(function(element) {
                 griglia.innerHTML += `
                 <div class="col-12 col-sm-6 col-lg-4 g-4">
-                    <div class="card h-100">
-                        <img src="${element.srcImg}" class="card-img-top" alt="${element.altImg}">
+                    <div class="card shadow h-100">
+                        <h5 class="card-title text-center my-3">${element.nome}</h5>
+                        <img src="${element.srcImg}" class="card-img-top rounded-0 border border-start-0 border-end-0" alt="${element.altImg}">
                         <div class="card-body">
-                            <h5 class="card-title">${element.nome}</h5>
                             <p class="card-text">${element.descrizione}</p>
-                            <div class="btn-group">
-                                <a href="${element.linkGithub}" target="_blank" class="btn btn-outline-primary">Guarda il codice</a>
-                                <a href="${element.linkSito}" target="_blank" class="btn btn-outline-primary">Guarda il sito</a>
-                            </div>
+                        </div>
+                        <div class="card-footer btn-group border-0">
+                            <a href="${element.linkGithub}" target="_blank" class="btn btn-outline-primary">Guarda il codice</a>
+                            <a href="${element.linkSito}" target="_blank" class="btn btn-outline-primary">Guarda il sito</a>
                         </div>
                     </div>
                 </div>`
