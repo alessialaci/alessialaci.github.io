a32e4a472a51d3ec01051e5475b44add706e78cb
Alessia Lacitignola <alelaci21@gmail.com>
2022-12-28 23:24:14 +0100

    graphic changes

--- assets/js/progetti.js
+++ assets/js/progetti.js
@@ -1,27 +1,30 @@
-var ultimiProgetti = [];
+var progetti = [];
 var griglia;
 
 window.addEventListener('DOMContentLoaded', init());
 
 function init() {
     griglia = document.getElementById('griglia');
-    printLastData();
+    printData();
 }
 
-function printLastData() {
+
+function printData() {
     fetch("assets/data/data.json").then((response) => {
         return response.json()
     }).then((data) => {
         progetti = data.progetti;
         if(progetti.length > 0) {
-            progetti.slice(-5, -1).reverse().map(function(element) {
+            progetti.reverse().map(function(element) {
                 griglia.innerHTML += `
-                <div class="col-12 col-sm-5 g-5">
-                    <div class="card h-100 text-light">
-                        <h5 class="card-title text-center fw-bold my-3">${element.nome}</h5>
-                        <div id="rettangolo"></div>
-                        <img src="${element.srcImg}" class="card-img-top rounded-0" alt="${element.altImg}">
-                        <div class="btn-group p-3">
+                <div class="col-12 col-sm-6 col-lg-4 g-4">
+                    <div class="card text-light shadow h-100">
+                        <h5 class="card-title text-center my-3">${element.nome}</h5>
+                        <img src="${element.srcImg}" class="card-img-top rounded-0 border border-start-0 border-end-0" alt="${element.altImg}">
+                        <div class="card-body">
+                            <p class="card-text">${element.descrizione}</p>
+                        </div>
+                        <div class="card-footer btn-group border-0">
                             <a href="${element.linkGithub}" target="_blank" class="btn btn-outline-success">Guarda il codice</a>
                             <a href="${element.linkSito}" target="_blank" class="btn btn-outline-success">Guarda il sito</a>
                         </div>
