d56f9ff1906758e636fdb8702c148c1eb1ec7634
Alessia Lacitignola <alelaci21@gmail.com>
2022-12-31 12:16:10 +0100

    Filter function added

--- assets/js/progetti.js
+++ assets/js/progetti.js
@@ -13,7 +13,7 @@ function printData() {
         return response.json()
     }).then((data) => {
         progetti = data.progetti;
-        if(progetti.length > 0) {
+        if (progetti.length > 0) {
             progetti.reverse().map(function(element) {
                 griglia.innerHTML += `
                 <div class="col-12 col-sm-6 col-lg-4 g-4">
@@ -30,10 +30,10 @@ function printData() {
                     </div>
                 </div>
                 `
-                
-                if (element.linkSito == '') {
+
+                if(element.linkSito == '') {
                     document.querySelectorAll('.sitoBtn').forEach(button => {
-                          button.classList.add('disabled');
+                        button.classList.add('disabled');
                     });
                 }
             })
@@ -47,54 +47,63 @@ let filtro = document.getElementById('filtro');
 
 function filterByCategories() {
     switch (filtro.value) {
-        case 'HTML': 
-            printCategory();
+        case 'Tutti':
+            printData();
             break;
-        case 'CSS': 
-            console.log('Ho scelto CSS');
+        case 'HTML':
+            printCategory('HTML');
             break;
-        case 'JavaScript': 
-            console.log('Ho scelto JavaScript');
+        case 'CSS':
+            printCategory('CSS');
             break;
-        case 'Sass': 
-            console.log('Ho scelto Sass');
+        case 'JavaScript':
+            printCategory('JavaScript');
             break;
-        case 'Bootstrap': 
-            console.log('Ho scelto Bootstrap');
+        case 'Sass':
+            printCategory('Sass');
             break;
-        case 'TypeScript': 
-            console.log('Ho scelto TypeScript');
+        case 'Bootstrap':
+            printCategory('Bootstrap');
             break;
-        case 'Angular': 
-            console.log('Ho scelto Angular');
+        case 'TypeScript':
+            printCategory('TypeScript');
+            break;
+        case 'Angular':
+            printCategory('Angular');
             break;
         default:
             console.log('Tutti');
     }
 }
 
-// function printCategory(id, category) {
-//     fetch(`assets/data/data.json/progetti/${id}/${category}`).then((response) => {
-//         return response.json()
-//     }).then((data) => {
-//         progettiFiltrati = data.includes('HTML') {
-//             griglia.innerHTML += `
-//                 <div class="col-12 col-sm-6 col-lg-4 g-4">
-//                     <div class="card text-light h-100">
-//                         <h5 class="card-title text-center fw-bold my-3" style="font-family: Arial, Helvetica, sans-serif;">${element.nome}</h5>
-//                         <img src="${element.srcImg}" class="card-img-top rounded-0 border border-start-0 border-end-0" alt="${element.altImg}">
-//                         <div class="card-body text-center">
-//                             <p class="card-text">${element.descrizione}</p>
-//                             <div class="btn-group p-3">
-//                                 <a href="${element.linkGithub}" target="_blank" class="btn btn-outline-success">Guarda il codice</a>
-//                                 <a href="${element.linkSito}" target="_blank" class="btn btn-outline-success sitoBtn">Guarda il sito</a>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 `
-//         }
-    
-// });
-
-// }
+function printCategory(categoria) {
+    fetch(`assets/data/data.json`).then((response) => {
+        return response.json()
+    }).then((data) => {
+        let progettiFiltrati = data.progetti.filter(progetto => progetto.categoria.includes(`${categoria}`));
+        
+        griglia.innerHTML = progettiFiltrati.reverse().map(function (element) {
+            return `
+            <div class="col-12 col-sm-6 col-lg-4 g-4">
+                <div class="card text-light h-100">
+                    <h5 class="card-title text-center fw-bold my-3" style="font-family: Arial, Helvetica, sans-serif;">${element.nome}</h5>
+                    <img src="${element.srcImg}" class="card-img-top rounded-0 border border-start-0 border-end-0" alt="${element.altImg}">
+                    <div class="card-body text-center">
+                        <p class="card-text">${element.descrizione}</p>
+                        <div class="btn-group p-3">
+                            <a href="${element.linkGithub}" target="_blank" class="btn btn-outline-success">Guarda il codice</a>
+                            <a href="${element.linkSito}" target="_blank" class="btn btn-outline-success sitoBtn">Guarda il sito</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+        }).join('');
+        
+        document.querySelectorAll('.sitoBtn').forEach(button => {
+            if(button.getAttribute('href') == '') {
+                button.classList.add('disabled');
+            }
+        });
+    });
+}
